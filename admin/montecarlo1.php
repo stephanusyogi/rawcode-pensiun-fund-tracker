4417f03dc831663bb3c9bfddadca58cf073c71a9
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-02-28 16:21:28 +0700

    Update montecarlo1.php

--- admin/montecarlo1.php
+++ admin/montecarlo1.php
@@ -1,1 +1,16 @@
-
+function umur_kini($tgl_lahir){
+			// ubah ke format Ke Date Time
+			$lahir = new DateTime($tgl_lahir);
+			$hari_ini = new DateTime();
+	
+			$diff = $hari_ini->diff($lahir);
+	
+			// Display
+			/*
+			echo "Tanggal Lahir: ". date('d M Y', strtotime($tgl_lahir)) .'<br />';
+			echo "Umur: ". $diff->y ." Tahun";
+			echo " ". $diff->m ." Bulan";
+			echo " ". $diff->d ." Hari";
+			*/
+			return array($diff->y,$diff->m,$diff->d);
+		}
