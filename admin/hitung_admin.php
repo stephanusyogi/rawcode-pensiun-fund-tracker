42b3c9649cf13f0f5b59c5fe39e4f248fe1f8f79
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-03-24 15:50:08 +0700

    Update hitung_admin.php

--- admin/hitung_admin.php
+++ admin/hitung_admin.php
@@ -24,7 +24,10 @@ for($i=1;$i<=$jml_port_ppip;$i++){
   $komposisi_likuid_depo_ppip[$i]=0.13; //Read komposisi deposito tranche likuiditas
   
   $return_ppip_inv[$i]= $komposisi_inv_saham_ppip[$i]*$return_saham_ppip[$i] + $komposisi_inv_fi_ppip[$i]*$return_fi_ppip[$i] + $komposisi_inv_depo_ppip[$i]*$return_depo_ppip[$i]; // perhitungan return portofolio tranche investasi
-  $return_ppip_likuid[$i]= $komposisi_likuid_saham_ppip[$i]*$return_saham_ppip[$i] + $komposisi_likuid_fi_ppip[$i]*$return_fi_ppip[$i] + $komposisi_likuid_depo_ppip[$i]*$return_depo_ppip[$i]; // perhitungan return portofolio tranche investasi
+  $return_ppip_likuid[$i]= $komposisi_likuid_saham_ppip[$i]*$return_saham_ppip[$i] + $komposisi_likuid_fi_ppip[$i]*$return_fi_ppip[$i] + $komposisi_likuid_depo_ppip[$i]*$return_depo_ppip[$i]; // perhitungan return portofolio tranche likuiditas
+  
+  $risk_ppip_inv_step1[$i]=$komposisi_inv_saham_ppip[$i]*$komposisi_inv_saham_ppip[$i]*$risk_saham_ppip[$i]*$risk_saham_ppip[$i] + $komposisi_inv_fi_ppip[$i]*$komposisi_inv_fi_ppip[$i]*$risk_fi_ppip[$i]*$risk_fi_ppip[$i] + $komposisi_inv_depo_ppip[$i]*$komposisi_inv_depo_ppip[$i]*$risk_depo_ppip[$i]*$risk_depo_ppip[$i];
+  $risk_ppip_inv_step2[$i]=
   
 }
 
