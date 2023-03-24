a0f954943d8f4c72de78a0fe7c8f36df77e8bfbb
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-03-24 14:57:15 +0700

    Update running_awal.php

--- admin/running_awal.php
+++ admin/running_awal.php
@@ -1,1 +1,20 @@
+<?php 
+  
+//A.1 Hitung Target Replacement Ratio
+  //Input: Read data Kuesioner
+    $nilai=0; //baca nilai di kuesioner
+    $jumlah=0;
+    for ($i=1;$i<=10;$i++){
+       $jumlah = $jumlah + $nilai;
+    }
+  
+  
+  //Output: Target Replacement
+//-------------------------------------------------
 
+//B.1 Hitung usia diangkat
+  //Input: Read
+      
+
+
+?>
