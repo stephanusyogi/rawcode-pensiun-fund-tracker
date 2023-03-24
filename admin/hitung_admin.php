4fe7e63131b7c4b5c6c5f6065d51397cf6c42ced
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-03-24 15:38:54 +0700

    Update hitung_admin.php

--- admin/hitung_admin.php
+++ admin/hitung_admin.php
@@ -1,1 +1,26 @@
+<?php 
+  
+//A.1. Hitung Return Portofolio PPIP
+//Input: Read Return, Risk, Korelasi, Komposisi tranche investasi, komposisi tranche likuiditas masing-masing portofolio
+$jml_port_ppip=4; //Read jumlah portofolio yang diinput oleh admin
+
+for($i=1;$i<=$jml_port_ppip;$i++){
+  //$i mencerminkan id dari portofolio
+  
+  $return_saham_ppip[$i]=0.13; //Read return saham
+  $return_fi_ppip[$i]=0.13; //Read return pendapatan tetap
+  $return_depo_ppip[$i]=0.13; //Read return deposito
+  
+  $risk_saham_ppip[$i]=0.13; //Read risk saham
+  $risk_fi_ppip[$i]=0.13; //Read risk pendapatan tetap
+  $risk_depo_ppip[$i]=0.13; //Read risk deposito
+  
+  
+  $return_ppip_investasi=$saham
+  
+}
+
+
+
+?>
 
