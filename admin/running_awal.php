49afc96cf23698e2d3a201ae4909796edea101fc
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-03-24 15:23:14 +0700

    Update running_awal.php

--- admin/running_awal.php
+++ admin/running_awal.php
@@ -7,14 +7,22 @@
     for ($i=1;$i<=10;$i++){
        $jumlah = $jumlah + $nilai;
     }
-  
-  
-  //Output: Target Replacement
+   
+  //Output: Create Data Target Replacement Ratio
 //-------------------------------------------------
 
 //B.1 Hitung usia diangkat
-  //Input: Read
-      
+  //Input: Read tanggal Lahir dan tanggal diangkat
+ $date1=date_create("1992-06-24"); //Read tanggal lahir
+ $date2=date_create("2018-02-06"); //Read tanggal diangkat
+ $diff=date_diff($date1,$date2);
+ 
+ $tahun=$diff->format('%y');
+ $bulan=$diff->format('%m');
+ 
+  //Output: Create data usia diangkat tahun = $tahun dan bulan =$bulan
+//----------------------------------------------------------------------
+
 
 
 ?>
