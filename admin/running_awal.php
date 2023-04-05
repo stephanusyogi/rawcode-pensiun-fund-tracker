cb3732e46388b2ae1d9e2ca1f686ab260c89d5b0
Arzizal <79443667+Arzizal@users.noreply.github.com>
2023-04-05 17:29:52 +0700

    Update running_awal.php

--- admin/running_awal.php
+++ admin/running_awal.php
@@ -61,14 +61,19 @@ for ($i=1;$i<=$jml;$i++){
   if($i==1){
     $tahun=$diff->format('%y');
     $bulan=$diff->format('%m');
-    //Output: Create $tahun dan $bulan ke masing-masing tahun dan bulan di database masa dinas
+    
+    $masa_dinas_tahun[$i]=$tahun;
+    $masa_dinas_bulan[$i]=$bulan;
+    //Output: Create $masa_dinas_tahun[$i] dan $masa_dinas_bulan[$i] ke masing-masing tahun dan bulan di database masa dinas
     $bulan = $bulan +1;
   } else {
     if($bulan >=12){
       $bulan = 1;
       $tahun = $tahun+1;
     }
-    //Output: Create $tahun dan $bulan ke masing-masing tahun dan bulan di database masa dinas
+    $masa_dinas_tahun[$i]=$tahun;
+    $masa_dinas_bulan[$i]=$bulan;
+    //Output: Create $masa_dinas_tahun[$i] dan $masa_dinas_bulan[$i] ke masing-masing tahun dan bulan di database masa dinas
     $bulan = $bulan +1;
   }
   
@@ -147,9 +152,11 @@ for ($i=1;$i<=10000;$i++){ //$i adalah primary key dari tabel normal inverse yan
 }
 
 //mulai perhitungan
+$z=1; //untuk konversi $flag_pensiun[$i] dari bulanan ke tahunan
 for ($i=1;$i<=$jml;$i++){
   $sisa_kerja_tahun[$i]=10;//Read sisa masa kerja tahun setiap bulan januari
-  $flag_pensiun[$i]=1;//Read flag pensiun setiap bulan januari
+  $flag_pensiun[$z]=1;//Read flag pensiun setiap bulan januari
+  $z=$z+12;
   
   //+++++++++++++++++++++++++++++++++
   //D.1., D.2., dan D.3. Hitung Montecarlo PPIP - hitung tranche, return, dan risk
@@ -411,6 +418,16 @@ $kode_input=($tahun*100)+$bulan; //untuk koding input
 
 $gaji_input=10000000; //Read gaji yang diinput
 $phdp_input=5000000; //Read phdp yang diinput
+$saldo_ppip_input=0; //numpang untuk mengisi saldo ppip, Read saldo ppip yang diinput
+$saldo_personal_keuangan_input=0;//numpang untuk mengisi saldo personal keuangan, Read saldo keuangan keuangan yang diinput
+$saldo_personal_properti_input=0;//numpang untuk mengisi saldo personal properti, Read saldo properti keuangan yang diinput
+
+//counter letak saldo ppip dan personal
+$counter_saldo_ppip=0;
+$counter_saldo_personal_keuangan=0;
+$counter_saldo_personal_properti=0;
+
+
 
 $gaji_naik=0.075;//Read kenaikan gaji di profile user
 $phdp_naik=0.05;//Read kenaikan phdp di profile user
@@ -425,6 +442,10 @@ for ($i=1;$i<=$jml;$i++){
 			$gaji[$i]=0;
 			$phdp[$i]=0;
 			
+			$saldo_ppip[$i]=0; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=0;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=0;//numpang untuk mengisi saldo personal properti
+			
 			$j=$j+1;
 			$k=1;
 			
@@ -432,6 +453,10 @@ for ($i=1;$i<=$jml;$i++){
 		} else{
 			$gaji[$i]=0;
 			$phdp[$i]=0;
+			
+			$saldo_ppip[$i]=0; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=0;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=0;//numpang untuk mengisi saldo personal properti
 			
 			$k=$k+1;
 			
@@ -442,6 +467,14 @@ for ($i=1;$i<=$jml;$i++){
 			$gaji[$i]=$gaji_input;
 			$phdp[$i]=$phdp_input;
 			
+			$saldo_ppip[$i]=$saldo_ppip_input; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=$saldo_personal_keuangan_input;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=$saldo_personal_properti_input;//numpang untuk mengisi saldo personal properti
+			
+			$counter_saldo_ppip=$i;
+			$counter_saldo_personal_keuangan=$i;
+			$counter_saldo_personal_properti=$i;
+			
 			$j=$j+1;
 			$k=1;
 			
@@ -449,6 +482,9 @@ for ($i=1;$i<=$jml;$i++){
 		} else{
 			$gaji[$i]=$gaji_input;
 			$phdp[$i]=$phdp_input;
+			$saldo_ppip[$i]=$saldo_ppip_input; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=$saldo_personal_keuangan_input;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=$saldo_personal_properti_input;//numpang untuk mengisi saldo personal properti
 			
 			$k=$k+1;
 			
@@ -459,6 +495,10 @@ for ($i=1;$i<=$jml;$i++){
 			$gaji[$i]=$gaji[$i-1]*(1+$gaji_naik);
 			$phdp[$i]=$phdp[$i-1]*(1+$phdp_naik);
 			
+			$saldo_ppip[$i]=0; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=0;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=0;//numpang untuk mengisi saldo personal properti
+			
 			$j=$j+1;
 			$k=1;
 			
@@ -466,15 +506,104 @@ for ($i=1;$i<=$jml;$i++){
 		} else{
 			$gaji[$i]=$gaji[$i-1];
 			$phdp[$i]=$phdp[$i-1];
+			$saldo_ppip[$i]=0; //numpang untuk mengisi saldo ppip
+			$saldo_personal_keuangan[$i]=0;//numpang untuk mengisi saldo personal keuangan
+			$saldo_personal_properti[$i]=0;//numpang untuk mengisi saldo personal properti
 			
 			$k=$k+1;
 			
 			$kode=($j*100)+$k;
 		}
 	}
-//jangan lupa menghapus $gaji dan $phdp dengan update data 
+//jangan lupa menghapus $gaji[$i] dan $phdp[$i] dengan update data 
 
 }
+
+//---------------------------------------------------------
+//F.2. Simulasi PPMP
+//Input: variabel $phdp[$i] yang ada di memory, Read masa dinas tahun dan bulan, dan flag pensiun
+
+$jml=936; // jumlah bulan dari januari 2023 s.d. desember 2100
+$date1=date_create("2018-02-06"); //Read tanggal diangkat
+$date2=date_create("2015-01-01"); //tanggal cutoff pensiun hybrid. yang diangkat setelah 1 januari 2015 ppip murni, kalau sebelumnya hybrid ppmp dan ppip
+$diff=date_diff($date1,$date2);
+
+$hari=$diff->format('%R%a');
+
+for ($i=1;$i<=$jml;$i++){
+	if ($hari>0){ //hybrid ppmp ppip
+		
+		$status_mp=1;//untuk hybrid ppmp ppip
+		
+		if ($flag_pensiun[$i]==0){ //belum pensiun
+			$masa_dinas=$masa_dinas_tahun[$i]+($masa_dinas_bulan[$i] / 12);
+			$jumlah_ppmp[$i]=0.025*$masa_dinas*$phdp[$i]; //rumus besar MP dalam PPMP
+			$rr_ppmp[$i]=$jumlah_ppmp[$i] / $gaji[$i]; //rumus mencari replacement ratio dalam ppmp
+			
+			//Output: create $jumlah_ppmp[$i] dan $rr_ppmp[$i]
+		
+		} else { //sudah pensiun
+			$jumlah_ppmp[$i]="null";
+			$rr_ppmp[$i]="null";
+			
+			//Output: create $jumlah_ppmp[$i] dan $rr_ppmp[$i]
+		}
+		
+	} else { //ppip murni
+		$status_mp=2;//untuk ppip murni
+		
+		$jumlah_ppmp[$i]="null";
+		$rr_ppmp[$i]="null";		
+		
+		//Output: create $jumlah_ppmp[$i] dan $rr_ppmp[$i]
+	}
+	
+}
+
+//---------------------------------------------------------
+//F.3. Simulasi PPIP
+//Input: variabel $gaji{$i] yang ada di memory serta flag pensiun, status mp yang sudah dihitung sebelumnya, Read tambahan iuran ppip, Read Saldo PPIP
+
+//F.3.1. Simulasi PPIP - Hitung iuran
+
+//menentukan besar iuran
+if ($status_mp==1){ //hybrid ppmp ppip
+	$persentase_iuran_ppip=0.09; //iuran ppip sebesar 9% untuk hybrid ppmp ppip
+} else {
+	$persentase_iuran_ppip=0.2; //iuran ppip sebesar 20% untuk ppip murni
+}
+
+
+//hitung iuran
+$jml=936; // jumlah bulan dari januari 2023 s.d. desember 2100
+$persentase_tambahan_iuran_ppip=0.1;// Read tambahan iuran ppip di profil user
+
+
+$j=1; //counter hasil investasi percentile monthly
+for ($i=1;$i<=$jml;$i++){
+	$iuran[$i]=$gaji[$i]*$persentase_iuran_ppip; //hitung besar iuran
+	$percentile_95_return_ppip_bulanan[$i]=$percentile_95_return_ppip[$j]; //menentukan percentile secara bulanan dari yang sebelumnya tahunan di monte carlo PPIP
+	$percentile_50_return_ppip_bulanan[$i]=$percentile_50_return_ppip[$j]; //menentukan percentile secara bulanan dari yang sebelumnya tahunan di monte carlo PPIP
+	$percentile_05_return_ppip_bulanan[$i]=$percentile_05_return_ppip[$j]; //menentukan percentile secara bulanan dari yang sebelumnya tahunan di monte carlo PPIP
+	
+	if (fmod($i,12)==0){
+		$j=$j+1;
+	}
+	$tambahan_iuran_ppip[$i]=$persentase_tambahan_iuran_ppip * $gaji[$i];
+	
+	
+	
+	
+	
+	
+	
+		
+		
+	
+}
+
+
+
 
 
 
